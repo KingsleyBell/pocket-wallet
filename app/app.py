2db65d6ce5bc4f74d012bf8d31eb809b35c6f6c6
Luke Bell <lukekingsleybell@gmail.com>
2019-08-08 13:50:41 +0200

    payfast integration.

--- app/app.py
+++ app/app.py
@@ -1,32 +1,68 @@
+import hashlib
 import json
 import os
+import urllib
 
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
 
 # the all-important app variable:
 application = Flask(__name__)
 
 
 @application.route('/')
-def home():
+def home(country='USA', promo=False):
     db_path = os.path.join(application.static_folder, 'catalogue/db.json')
     db = json.loads(open(db_path, 'r').read())
-    return render_template(
-        'index.html',
-        links=db,
-        promo=False
-    )
+    if request.method == 'POST':
+        quantity = int(request.form.get('quantity'))
+        name = request.form.get('name')
+        surname = request.form.get('surname')
+        email = request.form.get('email')
+        address1 = request.form.get('address1')
+        address2 = request.form.get('address2')
+        city = request.form.get('city')
+        country = request.form.get('country')
+        zip_code = request.form.get('zip-code')
+
+        post_data = {
+            'merchant_id': 11070985,
+            'merchant_key': '22ha4oa68qbxa',
+            'return_url': 'https://www.pocketwallet.co.za/order-complete/',
+            'cancel_url': 'https://www.pocketwallet.co.za/order-cancelled/',
+            'name_first': name,
+            'name_last': surname,
+            'email_address': email,
+            'amount': quantity * 150,
+            'item_name': f'Pocket Wallet (x {quantity})'
+        }
+
+        md5 = hashlib.md5(urllib.parse.urlencode(post_data).encode('utf-8')).hexdigest()
+        post_data['signature'] = md5
+
+        return jsonify(post_data)
+
+    else:
+        return render_template(
+            'index.html',
+            links=db,
+            promo=promo,
+            country=country
+        )
+
+
+@application.route('/US/')
+def us():
+    return home()
+
+
+@application.route('/ZA/', methods=['GET', 'POST'])
+def za():
+    return home(country='ZA')
 
 
 @application.route('/promo/')
 def promo():
-    db_path = os.path.join(application.static_folder, 'catalogue/db.json')
-    db = json.loads(open(db_path, 'r').read())
-    return render_template(
-        'index.html',
-        links=db,
-        promo=True
-    )
+    return home(promo=True)
 
 
 @application.route('/order-cancelled/')
--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
+  // Activate buy now button after page load
   $(".button-link").prop("disabled", false);
 
+  // Go to first section
   $("#home-link").click(function(e) {
     e.preventDefault();
     $(".nav-link")[0].click();
   });
 
+  // Section navigation
   $(".nav-link").click(function(e) {
     var targetId = e.target.id.split('-'),
     linkId = targetId[targetId.length - 1];
@@ -16,13 +19,48 @@ $(document).ready(function() {
     $(this).addClass("active");
   });
 
+  // Set global slide index
   $(".carousel").on('slid.bs.carousel', function () {
     var index = $(".section.active").find(".carousel-item.active").attr("index");
     $(".carousel-item.active").removeClass("active");
     $(".carousel-item[index=" + index + "]").addClass("active");
   });
 
+  // Promo watermark background
   Array.from(document.querySelectorAll('.watermarked')).forEach(function(el) {
         el.dataset.watermark = (el.dataset.watermark + ' ').repeat(10000);
+  });
+
+  // Country modal
+  $("#current-flag").click(function(e) {
+    $('#countryModal').modal();
+  });
+
+  // Order modal
+  $("#zar-button").click(function(e) {
+    $('#orderModal').modal();
+  });
+
+  // za order form submit
+  $("#za-order-form").submit(function(e) {
+
+      e.preventDefault(); // avoid to execute the actual submit of the form.
+
+      var form = $(this);
+      var url = window.location.pathname;
+
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: form.serialize(), // serializes the form's elements.
+        success: function(data)
+        {
+          $.each(data, function (key, val) {
+            $("#za-order-confirmation-form input[name=" + key + "]").val(val);
+            console.log(key, val);
+          });
+          $('#orderConfirmModal').modal();
+        }
+      });
   });
 });
